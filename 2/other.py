0c29e7275407af750b047ad2683cf1cd0158eaf7
Max Goryunov <maximthehedgehog@yandex.ru>
2024-09-24 16:22:43 +0300

    add plots

--- 2/other.py
+++ 2/other.py
@@ -1,25 +1,36 @@
 import math
+import statistics
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from scipy.stats import kendalltau
 
 # Step 1: Generate the data
 # np.random.seed(42)  # For reproducibility
 h = 0.05
-k_values = np.arange(501)
-normal_random_variables = np.random.normal(0, 1, 501)
-x_k = np.sqrt(k_values * h) + normal_random_variables
+k = np.arange(501)
+noise = np.random.normal(0, 1, 501)
+x_k = np.sqrt(k * h) + noise
+truth = np.sqrt(k * h)
 
 # Convert x_k to a DataFrame for easier manipulation
 df = pd.DataFrame(x_k, columns=['x_k'])
 
 
-def calculate_trends(df: pd.DataFrame, window_sizes):
+def calculate_trends(df, sizes):
     trends = {}
-    for window in window_sizes:
-        trends[f'moving_average_{window}'] = df['x_k'].rolling(window=window, min_periods=1, center=True).mean()
-        trends[f'moving_median_{window}'] = df['x_k'].rolling(window=window, min_periods=1, center=True).median()
+    for window in sizes:
+        temp = df['x_k']
+        val = df['x_k'].rolling(window=window, min_periods=1, center=True).mean()
+        val.iloc[0] = statistics.median([temp[0], temp[1], 3 * temp[1] - 2 * temp[2]])
+        # print(len(val), len(temp), val.iloc[-1], temp.iloc[-1])
+        val.iloc[-1] = statistics.median([temp.iloc[-1], temp.iloc[-2], 3 * temp.iloc[-2] - 2 * temp.iloc[-3]])
+        trends[f'moving_average_{window}'] = val
+        val = df['x_k'].rolling(window=window, min_periods=1, center=True).median()
+        val.iloc[0] = statistics.median([temp[0], temp[1], 3 * temp[1] - 2 * temp[2]])
+        val.iloc[-1] = statistics.median([temp.iloc[-1], temp.iloc[-2], 3 * temp.iloc[-2] - 2 * temp.iloc[-3]])
+        trends[f'moving_median_{window}'] = val
     return trends
 
 
@@ -62,4 +73,21 @@ for key, trend in trends.items():
         print(f"Ряд остатков {key} по коэффициенту Кендала случаен")
     else:
         print(f"Ряд остатков {key} по коэффициенту Кендала не случаен")
+
+
+# Визуализация результатов
+plt.figure(figsize=(14, 8))
+plt.plot(x_k, label="Исходный ряд", color='gray', alpha=0.6, linestyle='dashed')
+plt.plot(truth, label="Истинный корень", color='red', alpha=0.6, linestyle='dashed')
+
+
+for key, trend in trends.items():
+    plt.plot(trend, label=f"{key}")
+
+
+
+plt.title("Тренды по скользящим средним и медианам")
+plt.legend()
+plt.grid()
+plt.show()
 
